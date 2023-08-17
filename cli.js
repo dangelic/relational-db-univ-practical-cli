dd11aa1a96ff11143ec24fc1e11e2d0f9d393fcd
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-08-17 13:43:09 +0200

    #feat Add cli category tree and path search

--- cli.js
+++ cli.js
@@ -1,20 +1,45 @@
 const Table = require('cli-table3');
 const readlineSync = require('readline-sync');
 
-const BASE_URL = 'http://localhost:8080/api/v1/products/top';
 
-async function fetchTopProducts(k) {
-    try {
-      const response = await fetch(`${BASE_URL}?k=${k}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching top products:', error.message);
-      return [];
-    }
+const BASE_URL = 'http://localhost:8079/api/v1';
+
+const fetchTopProducts = async (k) => {
+  try {
+    const response = await fetch(`${BASE_URL}/products/top?k=${k}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching top products:', error.message);
+    return [];
   }
+};
 
-function displayProductsTable(products) {
+const fetchCategoryTree = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/categories/tree`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching category tree:', error.message);
+    return [];
+  }
+};
+
+
+
+const fetchProductsByCategoryPath = async (categoryPath) => {
+  try {
+    const response = await fetch(`${BASE_URL}/categories/products?categoryPath=${encodeURIComponent(categoryPath)}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching products by category path:', error.message);
+    return [];
+  }
+};
+
+const displayProductsTable = (products) => {
   const table = new Table({
     head: ['ASIN', 'Product Title', 'Product Group', 'EAN', 'Sales Rank', 'Average Rating'],
   });
@@ -31,45 +56,84 @@ function displayProductsTable(products) {
   });
 
   console.log(table.toString());
+};
+
+function displayCategoryTree(categories, depth = 0) {
+  categories.forEach((category, index, array) => {
+    const isLast = index === array.length - 1;
+    const prefix = depth > 0 ? (isLast ? '└── ' : '├── ') : '';
+
+    const indent = '    '.repeat(depth); // Four spaces for each level of depth
+
+    console.log(`${indent}${prefix}${category.name}`);
+    
+    if (category.subcategories.length > 0) {
+      displayCategoryTree(category.subcategories, depth + 1);
+    }
+  });
 }
 
-function main() {
-  console.log('Welcome to the Top Product Viewer!');
-  console.log('---------------------------------');
 
-  const options = ['List top K products', 'Exit'];
-  const selectedOption = readlineSync.keyInSelect(options, 'Choose an option:', {
-    cancel: 'Exit',
-  });
+const main = async () => {
+  console.log('Welcome to the Product Viewer CLI!');
+  console.log('----------------------------------');
 
-  if (selectedOption === -1) {
-    console.log('Goodbye!');
-    process.exit(0);
-  }
+  while (true) {
+    const options = ['List top K products', 'Display category tree', 'Fetch products by category path', 'Exit'];
+    const selectedOptionIndex = readlineSync.keyInSelect(options, 'Choose an option:', {
+      cancel: 'Exit',
+    });
 
-  if (selectedOption === 0) {
-    const k = parseInt(readlineSync.question('Enter the value of K: '));
-
-    if (isNaN(k) || k <= 0) {
-      console.error('Invalid input. Please enter a positive integer for K.');
-      return main();
+    if (selectedOptionIndex === -1) {
+      console.log('Goodbye!');
+      break;
     }
 
-    fetchTopProducts(k)
-      .then((products) => {
+    switch (selectedOptionIndex) {
+      case 0: {
+        const k = parseInt(readlineSync.question('Enter the value of K: '));
+
+        if (isNaN(k) || k <= 0) {
+          console.error('Invalid input. Please enter a positive integer for K.');
+          continue;
+        }
+
+        const products = await fetchTopProducts(k);
         if (products.length === 0) {
           console.log('No products found.');
         } else {
           console.log('Top Products:');
           displayProductsTable(products);
         }
-        main();
-      })
-      .catch((error) => {
-        console.error('Error:', error.message);
-        main();
-      });
-  }
-}
+        break;
+      }
 
-main();
+      case 1: {
+        const categories = await fetchCategoryTree();
+        console.log('Category Tree:');
+        displayCategoryTree(categories);
+        break;
+      }
+
+      case 2: {
+        const categoryPath = readlineSync.question('Enter the category path: ');
+        const products = await fetchProductsByCategoryPath(categoryPath);
+        if (products.length === 0) {
+          console.log('No products found.');
+        } else {
+          console.log('Products for Category Path:');
+          displayProductsTable(products);
+        }
+        break;
+      }
+
+      default:
+        console.log('Invalid option.');
+        break;
+    }
+  }
+};
+
+main().catch((error) => {
+  console.error('Error:', error.message);
+});
